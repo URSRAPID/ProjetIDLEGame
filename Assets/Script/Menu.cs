e0eb58ac10bfe0836b7e03030d50b17cd2102d52
URSRAPID <98385291+URSRAPID@users.noreply.github.com>
2022-04-05 12:46:47 +0200

    FirstCommit

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ButtonPaly()
+    {
+        SceneManager.LoadScene("SceneGame");
+    }
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+}
