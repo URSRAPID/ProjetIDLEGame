4b195b2aaa455b40975b3e103fd2aeb734341454
URSRAPID <98385291+URSRAPID@users.noreply.github.com>
2022-04-12 10:37:59 +0200

    ListWaypoints

--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
+    public List <GameObject> _waypointsList1;
+    public List<GameObject> _waypointsList2;
+    public List<GameObject> _waypointsList3;
+    public List<GameObject> _waypointsList4;
+    public List<GameObject> _waypointsList5;
 
     public GameObject[] waypoints;
     int current = 0;
@@ -12,17 +17,24 @@ public class Waypoints : MonoBehaviour
     float WPradius = 1;
     public bool stop;
 
+    
+
+    private void Start()
+    {
+        
+
+    }
     void Update()
     {
         if (!stop)
         {
-            if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+            if (Vector3.Distance(_waypointsList1[current].transform.position, transform.position) < WPradius)
             {
 
                 current++;
 
             }
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, _waypointsList1[current].transform.position, Time.deltaTime * speed);
         }
     }
 }
